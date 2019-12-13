ed29f9969e545ef26f3c0badc1573bc4068aa2d3
Ellie <elliemmanifold@gmail.com>
2019-12-13 16:08:28 +0000

    Try out sessions in app file

--- app.rb
+++ app.rb
@@ -3,7 +3,20 @@ require 'json'
 
 set :public_folder, proc { File.join(root) }
 
-get '/index.html' do
-  # headers 'Access-Control-Allow-Origin' => '*'
-  # { time: Time.now.to_s}.to_json
+class Thermostat < Sinatra::Base
+  enable :sessions
+  get '/index.html' do
+    # headers 'Access-Control-Allow-Origin' => '*'
+    # { time: Time.now.to_s}.to_json
+    index = session[:index] || 0.to_s
+
+    content_type :json
+    { index: index }.to_json
+  end
+
+  post '/index.html' do
+    session[:index] = params[:index]
+    200
+  end
+
 end
